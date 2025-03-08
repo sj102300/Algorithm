5e76a143fd276a9931f4febe988840222e7dbca3
seungJoo <sj10230506@gmail.com>
2025-03-08 14:22:26 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/42626

---
+++ 구현/더맵게.cc
@@ -0,0 +1,37 @@
+//[Lv2] 더맵게 https://school.programmers.co.kr/learn/courses/30/lessons/42626
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    
+    priority_queue pq{greater<int>{}, scoville};
+    
+    int first;
+    int second;
+    int mixValue;
+    int mixCnt = 0;
+    while(pq.size() > 1){
+        first = pq.top();
+        if(first >= K){
+            return mixCnt;
+        }
+        pq.pop();
+        second = pq.top();
+        pq.pop();
+        ++mixCnt;
+        mixValue = first + second * 2;
+        pq.push(mixValue);
+    }
+    
+    if(pq.top() >= K){
+        return mixCnt;
+    }
+    else{
+        return -1;
+    }
+}
