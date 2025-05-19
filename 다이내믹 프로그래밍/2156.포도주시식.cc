d08f84adb6ac84ded49a1a972d5c43f2e0bb3c3b
seungJoo <sj10230506@gmail.com>
2025-05-19 17:15:58 +0900

    [실버1] 2156.포도주시식 https://www.acmicpc.net/problem/2156

---
+++ 다이내믹 프로그래밍/2156.포도주시식.cc
@@ -0,0 +1,38 @@
+//[실버1] 2156.포도주시식 https://www.acmicpc.net/problem/2156
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int juice[10001] = {0};
+int dp[10001][3] = {0};
+
+void getInput(){
+    cin>>n;
+
+    for(int i=0;i<n;++i){
+        cin>>juice[i];
+    }
+}
+
+int main(void){
+    freopen("/root/LigNex1/Algorithm/cpp/input.txt", "r", stdin);
+
+    getInput();
+
+    dp[0][0] = juice[0];
+
+    for(int i=1;i<n;++i){
+        dp[i][0] = dp[i-1][2] + juice[i];
+        dp[i][1] = dp[i-1][0] + juice[i];
+        dp[i][2] = max(dp[i-1][2], max(dp[i-1][1], dp[i-1][0]));
+    }
+
+    int answer = 0;
+    for(int i=0;i<3;++i){
+        answer = max(answer, dp[n-1][i]);
+    }
+    cout<<answer;
+}
